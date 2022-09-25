f35862adcd51072f1d524e234fafae945deffc02
Changhua <lichanghua0821@gmail.com>
2022-09-25 20:14:58 +0800

    Add comments

--- main.py
+++ main.py
@@ -13,8 +13,10 @@ class Robot(BaseRobot):
         super().__init__(sdk, config)
 
     def processMsg(self, msg) -> None:
-        # 如果不实现本方法，则默认打印原始消息
+        """当接收到消息的时候，会调用本方法。如果不实现本方法，则打印原始消息。
+        """
 
+        # 如果在群里被 @，回复发信人：“收到你的消息了！” 并 @他
         if self.isGroupChat(msg):  # 是群消息
             if self.isAt(msg):   # 被@
                 if msg.roomId in self.config.GROUPS:  # 在配置的响应的群列表里
