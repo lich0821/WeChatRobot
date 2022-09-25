b8d1e3bafbb4a0abba63f572ddd762759c94e31a
Changhua <lichanghua0821@gmail.com>
2022-09-25 20:28:12 +0800

    Show received messages

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ class Robot(BaseRobot):
         """当接收到消息的时候，会调用本方法。如果不实现本方法，则打印原始消息。
         """
 
+        self.printRawMsg(msg)  # 打印信息
         # 如果在群里被 @，回复发信人：“收到你的消息了！” 并 @他
         if self.isGroupChat(msg):  # 是群消息
             if self.isAt(msg):   # 被@
