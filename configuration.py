78fa5035a1f13e47994c1de8839ef941f598ed7d
Changhua <lichanghua0821@gmail.com>
2023-07-08 07:36:00 +0800

    Add TigerBot

--- configuration.py
+++ configuration.py
@@ -30,3 +30,4 @@ class Config(object):
         self.GROUPS = yconfig["groups"]["enable"]
         self.CHATGPT = yconfig.get("chatgpt")
         self.NEWS = yconfig["news"]["receivers"]
+        self.TIGERBOT = yconfig.get("tigerbot")
