d1c1e2ca4351162c621849e27caf9469cb6c8d3e
TechShrimp <939178127@qq.com>
2023-10-15 15:12:12 +0800

    AutoPEP8

--- func_xinghuo_web.py
+++ func_xinghuo_web.py
@@ -2,19 +2,24 @@
 # -*- coding: utf-8 -*-
 from sparkdesk_web.core import SparkWeb
 
+
 class XinghuoWeb():
     def __init__(self, xhconf=None) -> None:
 
         self._sparkWeb = SparkWeb(
             cookie=xhconf["cookie"],
             fd=xhconf["fd"],
-            GtToken=xhconf["GtToken"]
+            GtToken=xhconf["GtToken"],
         )
         self._chat = self._sparkWeb.create_continuous_chat()
+        # 如果有提示词
+        if xhconf["prompt"]:
+            self._chat.chat(xhconf["prompt"])
 
     def get_answer(self, msg: str, sender: str = None) -> str:
         answer = self._chat.chat(msg)
         return answer
+
 
 if __name__ == "__main__":
     from configuration import Config
