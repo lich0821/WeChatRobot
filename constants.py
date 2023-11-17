d3f715271047e0adc27368bb44e970ffbc00ceb1
weiensong <wes@shoot.net.cn>
2023-11-17 11:47:49 +0800

    fix: constants name

--- constants.py
+++ constants.py
@@ -3,20 +3,20 @@ from enum import IntEnum, unique
 
 @unique
 class ChatType(IntEnum):
-    UnKnown = 0  # 未知, 即未设置
+    # UnKnown = 0  # 未知, 即未设置
     TIGER_BOT = 1  # TigerBot
     CHATGPT = 2  # ChatGPT
     XINGHUO_WEB = 3  # 讯飞星火
-    ChatGLM = 4  # ChatGLM
+    CHATGLM = 4  # ChatGLM
 
     @staticmethod
     def is_in_chat_types(chat_type: int) -> bool:
         if chat_type in [ChatType.TIGER_BOT.value, ChatType.CHATGPT.value,
-                         ChatType.XINGHUO_WEB.value, ChatType.ChatGLM.value]:
+                         ChatType.XINGHUO_WEB.value, ChatType.CHATGLM.value]:
             return True
         return False
 
     @staticmethod
-    def help_hint():
+    def help_hint() -> str:
         return str({member.value: member.name for member in ChatType}).replace('{', '').replace('}', '')
 
