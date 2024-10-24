91c01465e25ba651f32b815abe42d09dad9ed9d4
xiongpan <xiongpan@nercrm.com>
2024-10-24 18:21:06 +0800

    怎么宝箱图片过滤

--- img_ocr.py
+++ img_ocr.py
@@ -72,7 +72,7 @@ def unclaimed(data):
 
 
 # 计算宝箱积分并排序
-def sort_and_filter_unique_x_texts(data):
+def sort_and_filter_x_texts(data):
     def get_min_x(detect):
         return min(point['X'] for point in detect['Polygon'])
 
@@ -82,58 +82,63 @@ def sort_and_filter_unique_x_texts(data):
     x_texts = list(filter(is_x_text, data))
     sorted_detections = sorted(x_texts, key=get_min_x)
 
-    seen = set()
-    unique_sorted_x_texts = []
+    sorted_x_texts = [detection['DetectedText'] for detection in sorted_detections]
 
-    for detection in sorted_detections:
-        text = detection['DetectedText']
-        if text not in seen:
-            seen.add(text)
-            unique_sorted_x_texts.append(text)
+    # 返回第 0、1、2、4 个元素
+    return [sorted_x_texts[i] for i in [0, 1, 2, 4] if i < len(sorted_x_texts)]
 
-    return unique_sorted_x_texts
+
+# 过滤非宝箱图片
+def contains_all_keywords_combined(text_detections, keywords):
+    combined_text = " ".join(detection['DetectedText'] for detection in text_detections)
+    return all(keyword in combined_text for keyword in keywords)
 
 
 # 宝箱积分计算
 def count_box(response):
-    # 宝箱积分
-    sorted_unique_x_texts = sort_and_filter_unique_x_texts(response['TextDetections'])
+    # 过滤response内容关键字
+    keywords = ["宝箱", "积分领取", "抽到紫将概率", "打开", "个宝箱"]
+    if contains_all_keywords_combined(response['TextDetections'], keywords):
+        # 宝箱积分
+        sorted_unique_x_texts = sort_and_filter_x_texts(response['TextDetections'])
 
-    wood, bronze, gold, platinum = [text.replace('X', '') for text in sorted_unique_x_texts[:4]]
+        wood, bronze, gold, platinum = [text.replace('X', '') for text in sorted_unique_x_texts[:4]]
 
-    # 未使用积分
-    points_unclaimed = unclaimed(response["TextDetections"])
+        # 未使用积分
+        points_unclaimed = unclaimed(response["TextDetections"])
 
-    points_wood = points(wood, 1)
-    points_bronze = points(bronze, 10)
-    points_gold = points(gold, 20)
-    points_platinum = points(platinum, 50)
+        points_wood = points(wood, 1)
+        points_bronze = points(bronze, 10)
+        points_gold = points(gold, 20)
+        points_platinum = points(platinum, 50)
 
-    points_all = points_wood + points_bronze + points_gold + points_platinum
+        points_all = points_wood + points_bronze + points_gold + points_platinum
 
-    points_no_wood = points_bronze + points_gold + points_platinum
-    points_no_platinum = points_wood + points_bronze + points_gold
+        points_no_wood = points_bronze + points_gold + points_platinum
+        points_no_platinum = points_wood + points_bronze + points_gold
 
-    msg = ("木质箱子: {}个 --- 积分: {}\n"
-           "青铜箱子: {}个 --- 积分: {}\n"
-           "黄金箱子: {}个 --- 积分: {}\n"
-           "铂金箱子: {}个 --- 积分: {}\n"
-           "原始积分: {}\n"
-           "未领取累计积分: {}\n"
-           "宝箱周: {:.2f}轮\n"
-           "不开木箱: {:.2f}轮\n"
-           "不开铂金: {:.2f}轮\n"
-           "------------------".format(wood,
-                                      points_wood,
-                                      bronze,
-                                      points_bronze,
-                                      gold,
-                                      points_gold,
-                                      platinum,
-                                      points_platinum,
-                                      points_all,
-                                      points_unclaimed,
-                                      points_all / 3340,
-                                      points_no_wood / 3340,
-                                      points_no_platinum / 3340))
-    return msg
+        msg = ("木质箱子: {}个 --- 积分: {}\n"
+               "青铜箱子: {}个 --- 积分: {}\n"
+               "黄金箱子: {}个 --- 积分: {}\n"
+               "铂金箱子: {}个 --- 积分: {}\n"
+               "原始积分: {}\n"
+               "未领取累计积分: {}\n"
+               "宝箱周: {:.2f}轮\n"
+               "不开木箱: {:.2f}轮\n"
+               "不开铂金: {:.2f}轮\n"
+               "------------------".format(wood,
+                                           points_wood,
+                                           bronze,
+                                           points_bronze,
+                                           gold,
+                                           points_gold,
+                                           platinum,
+                                           points_platinum,
+                                           points_all,
+                                           points_unclaimed,
+                                           points_all / 3340,
+                                           points_no_wood / 3340,
+                                           points_no_platinum / 3340))
+        return msg
+    else:
+        return None
